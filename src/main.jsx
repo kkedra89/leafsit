8fc0e90030419ae9e45bfcb3dfbee35b295eb763
kkedra89 <krystian.kedra@icloud.com>
2026-08-18 22:24:07 +0200

    Dodano Sentry do sledzenia bledow

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App.jsx'
+
+Sentry.init({
+  dsn: "https://a43c25b500f0ecf12bd4813eb8601f1f@o4511933625663488.ingest.de.sentry.io/4511933646831696",
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
